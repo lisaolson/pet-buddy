1a3234ff3ff93f673b70c9a3ce74cd88d2d6d9e2
Lisa Olson <olson.lisa94@gmail.com>
2018-10-04 21:52:59 -0700

    Adds View controllers for messages and updates individual cell view

---
+++ petBuddy/Controller/SingleMessageViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SingleMessageViewController.swift
+//  petBuddy
+//
+//  Created by Lisa Olson on 10/4/18.
+//  Copyright © 2018 Lisa Olson. All rights reserved.
+//
+
+import UIKit
+
+class SingleMessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
